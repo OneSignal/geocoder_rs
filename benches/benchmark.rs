db88b5eb707347dc4d49b1826eef7ce769ff097b
Josh Leverette <jleverette@onesignal.com>
2018-06-05 17:03:20 -0700

    fix benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,19 +1,19 @@
 #[macro_use]
 extern crate criterion;
 
-extern crate geocoder;
+extern crate geocoder_rs;
 
 use criterion::Criterion;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let _len = geocoder::COUNTRIES.len();
-    let coord = geocoder::Point::new(-122.3164206, 37.5506619);
-    let coord_slow = geocoder::Point::new(-75.9445286, 37.3511739);
+    let _len = geocoder_rs::COUNTRIES.len();
+    let coord = geocoder_rs::Point::new(-122.3164206, 37.5506619);
+    let coord_slow = geocoder_rs::Point::new(-75.9445286, 37.3511739);
     c.bench_function("find_country_fast", move |b| {
-        b.iter(|| geocoder::find_country(&coord))
+        b.iter(|| geocoder_rs::find_country(&coord))
     });
     c.bench_function("find_country_slow", move |b| {
-        b.iter(|| geocoder::find_country(&coord_slow))
+        b.iter(|| geocoder_rs::find_country(&coord_slow))
     });
 }
 
